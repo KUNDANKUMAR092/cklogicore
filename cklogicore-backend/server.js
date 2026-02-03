b6b1f50db40977ce0a0f459825fb132cf1a2ca43
Kundan Kumar Singh <kundan.kumar.singh092@gmail.com>
2026-02-03 22:53:43 +0530

    fix: Final path correction

--- cklogicore-backend/server.js
+++ cklogicore-backend/server.js
@@ -22,7 +22,7 @@ connectDB();
 
 // Frontend build folder ka path (Parent folder se cklogicore-frontend/dist tak)
 // const frontendBuildPath = path.join(__dirname, "../cklogicore-frontend/dist");
-const frontendBuildPath = path.resolve(__dirname, "../cklogicore-frontend/dist");
+const frontendBuildPath = path.resolve(__dirname, "../dist");
 
 app.use(express.static(frontendBuildPath));
 
